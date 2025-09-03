5b33c1a0e0ce69c238c72ee60d706f68f8e345e9
Svetlana <pl.swetik@yandex.ru>
2025-09-03 14:10:10 +0400

    Добавлена работа с каталогом

--- FileSearchWord/Program.cs
+++ FileSearchWord/Program.cs
@@ -4,15 +4,26 @@ namespace FileSearch
 {
     internal class Program
     {
-        public static List<string> FileSearch(string path)
-        {            
-            string[] files = Directory.GetFiles(path);
+        public static List<string> FileSearch(string catalog)
+        {
+            string[] files;
             List<string> fileNames = new List<string>();
-            foreach (string fileName in files)
-            {                    
-                fileNames.Add(fileName);
-            }            
-            return fileNames;            
+            try
+            {
+                files = Directory.GetFiles(catalog);
+                if (files.Length > 0)
+                {
+                    foreach (string fileName in files)
+                    {
+                        fileNames.Add(fileName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }           
+            return fileNames;
         }
         public static List<int> AllIndexesOf(string content, string word)
         {            
@@ -40,12 +51,16 @@ namespace FileSearch
         static void Main(string[] args)
         {
             bool work = true;
+            
             while (work == true)
             {
                 string word = string.Empty;
                 while (word == string.Empty)
                 {
-                    Console.WriteLine("Введите слово, для выхода из программы нажмите f");
+                    string catalog = "C:\\Users\\user\\source\\repos\\fileSearch\\FileSearchWord\\bin\\Debug\\net8.0";
+                    Console.WriteLine("Введите название каталога");
+                    catalog = Console.ReadLine();
+                    Console.WriteLine("Введите слово для поиска, для выхода из программы нажмите f");
                     word = Console.ReadLine();
                     if (word != string.Empty)
                     {
@@ -55,19 +70,27 @@ namespace FileSearch
                         }
                         else
                         {
-                            List<string> files = FileSearch("Сказки");
-                            Dictionary<string, List<int>> occurrencesWord = new Dictionary<string, List<int>>();
-                            List<Task<Dictionary<string, List<int>>>> tasks = new List<Task<Dictionary<string, List<int>>>>();
-                            foreach (string fileName in files)
+                            List<string> files = FileSearch(catalog);
+                            if (files.Count() > 0) 
                             {
-                                tasks.Add(Task.Run(() => SreahWord(fileName, word, occurrencesWord)));
-                                Console.WriteLine();
+                                Dictionary<string, List<int>> occurrencesWord = new Dictionary<string, List<int>>();
+                                List<Task<Dictionary<string, List<int>>>> tasks = new List<Task<Dictionary<string, List<int>>>>();
+                                foreach (string fileName in files)
+                                {
+                                    tasks.Add(Task.Run(() => SreahWord(fileName, word, occurrencesWord)));
+                                    Console.WriteLine();
+                                }
+                                Task.WaitAll(tasks.ToArray());
+                                foreach (var sreahWord in occurrencesWord)
+                                {
+                                    Console.WriteLine($" В файле {sreahWord.Key} найдено совпадений {sreahWord.Value.Count()}");
+                                }
                             }
-                            Task.WaitAll(tasks.ToArray());
-                            foreach (var sreahWord in occurrencesWord)
-                            {
-                                Console.WriteLine($" В файле {sreahWord.Key} найдено совпадений {sreahWord.Value.Count()}");                          
+                            else 
+                            { 
+                                Console.WriteLine("Каталог пуст"); 
                             }
+                            
                         }
                         
                     }
